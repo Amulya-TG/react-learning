bba4945e8ad80948fe55348bced211f6768c2dca
Amulya-TG <rohinigangadhar28@gmail.com>
2026-03-27 11:03:10 +0530

    Style todo list app with CSS

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,41 +1,45 @@
 import React, { useState } from "react";
 
-const TodoList = () =>{
-  const[todo,setTodo]=useState("");
-  const[tasks,setTask]=useState([])
+const TodoList = () => {
+  const [todo, setTodo] = useState("");
+  const [tasks, setTask] = useState([]);
 
-    function handelClick(){
-      setTask([...tasks,todo]);
-      setTodo("")
-    }
+  function handelClick() {
+    setTask([...tasks, todo]);
+    setTodo("");
+  }
 
-    function handelDel(index){
-      console.log(index);
-      
-        const newTask = tasks.filter((task,i)=>i!==index)
-        console.log(i);
-        
-        setTask(newTask)
-    }
-  return(
-    <div>
-      <div>
-        <input type="text"
-        onChange={(e)=>setTodo(e.target.value)}
-        value={todo}
-        placeholder="Enter task"/>
-      <button type="submit" onClick={handelClick}>Add</button>
+  function handelDel(index) {
+    console.log(index);
+
+    const newTask = tasks.filter((task, i) => i !== index);
+    console.log(i);
+
+    setTask(newTask);
+  }
+  return (
+    <div className="container">
+      <div className="in-add">
+        <input
+          type="text"
+          onChange={(e) => setTodo(e.target.value)}
+          value={todo}
+          placeholder="Enter task"
+        />
+        <button type="submit" onClick={handelClick}>
+          Add
+        </button>
       </div>
-      <div>
-        {tasks.map((task,index)=>(
+      <div className="task-add">
+        {tasks.map((task, index) => (
           <div key={index}>
             <h2>{task}</h2>
-            <button onClick={()=>handelDel(index)}>Delete</button>
+            <button onClick={() => handelDel(index)}>Delete</button>
           </div>
         ))}
       </div>
     </div>
-  )
-}
- 
+  );
+};
+
 export default TodoList;
