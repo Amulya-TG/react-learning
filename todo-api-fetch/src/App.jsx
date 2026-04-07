3494a87134564ac41fff04dfd451f76c9d033fdf
Amulya-TG <rohinigangadhar28@gmail.com>
2026-04-07 12:31:17 +0530

    integrated API data with existing todo components

--- todo-api-fetch/src/App.jsx
+++ todo-api-fetch/src/App.jsx
@@ -1,20 +1,21 @@
 import React, { useState, useEffect } from "react";
+import Complete from "./components/Complete";
 
 const TodoFetch = () => {
   const [todos, setTodos] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const fetchTodo = async () => {
-    try{
-      const res = await fetch("https://jsonplaceholder.typicode.com/todos");
-    const data = await res.json();
-    setTodos(data.slice(0, 10));
-    }catch(error){
-      console.log(error);
-      
-    }
-  };
   useEffect(() => {
+    const fetchTodo = async () => {
+      try {
+        const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+        const data = await res.json();
+        setTodos(data.slice(0, 10));
+        console.log(data.slice(0, 10));
+      } catch (error) {
+        console.error("Something went wrong", error);
+      }
+    };
     setLoading(true);
     fetchTodo();
     setLoading(false);
@@ -24,10 +25,14 @@ const TodoFetch = () => {
       {loading ? (
         <p>loading....</p>
       ) : (
-        <div>
+        <div className="todo-list">
+          <h1>Todos List</h1>
+          <ul>
           {todos.map((todo, index) => (
-            <h3 key={index}>{todo.title}</h3>
+            <li key={index}>{todo.title}</li>
           ))}
+          </ul>
+          <Complete todos={todos} />
         </div>
       )}
     </>
