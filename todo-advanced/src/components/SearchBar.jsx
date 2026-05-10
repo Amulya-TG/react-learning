15fb9a5867f1340da12e90d221374b1b6ce960be
Amulya-TG <rohinigangadhar28@gmail.com>
2026-05-10 21:44:16 +0530

    Add search functionality for todos

---
+++ todo-advanced/src/components/SearchBar.jsx
@@ -0,0 +1,13 @@
+const SearchBar = ({search,setSearch}) => {
+    return(
+        <>
+            <input type="text"
+            className="search"
+            placeholder="serach todo"
+            value={search}
+            onChange={(e)=>setSearch(e.target.value)} />    
+        </>
+    )
+}
+
+export default SearchBar;
