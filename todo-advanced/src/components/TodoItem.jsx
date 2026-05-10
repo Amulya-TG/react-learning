f1cb8cac7927a65f53ef80f3cb6c263a1d747944
Amulya-TG <rohinigangadhar28@gmail.com>
2026-05-10 21:43:00 +0530

    Add todo item component with edit toggle and delete features

---
+++ todo-advanced/src/components/TodoItem.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+const TodoItem = ({ todo, toggleTodo, handelDel, handelEdit }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [newText, setNewText] = useState(todo.text);
+  function handelSave(){
+    handelEdit(todo.id,newText);
+    setIsEditing(false);
+  }
+
+  return (
+    <div className="todo-item">
+      {isEditing ? (
+        <>
+          <input
+            type="text"
+            value={newText}
+            onChange={(e) => setNewText(e.target.value)}
+          />
+          <button onClick={handelSave}>Save</button>
+        </>
+      ) : (
+        <>
+          <span>{todo.title}</span>
+          <div className="btns">
+            <button onClick={() => toggleTodo(todo.id)}>
+              {todo.completed ? "mark Incomplete" : "Mark Complete"}
+            </button>
+            <button onClick={()=>setIsEditing(true)}>Edit</button>
+            <button onClick={()=>handelDel(todo.id)}>Delete</button>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default TodoItem;
